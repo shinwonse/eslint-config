b776ec2c4f2710e702358f44664bccaed998189b
신원세/프론트엔드개발팀 <wsshin@ktown4u.com>
2024-06-26 17:03:48 +0900

    버전 업데이트

--- eslint-config-react/index.js
+++ eslint-config-react/index.js
@@ -2,20 +2,26 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:jsx-a11y/recommended',
     'plugin:import/recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['simple-import-sort', 'sort-destructure-keys', 'sort-keys-fix'],
+  plugins: [
+    'simple-import-sort',
+    'sort-destructure-keys',
+    'sort-keys-fix',
+    'typescript-sort-keys',
+  ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
 
     'sort-destructure-keys/sort-destructure-keys': 'error',
-
     'sort-keys-fix/sort-keys-fix': 'error',
 
-    'react/react-in-jsx-scope': 'off',
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'error',
   },
 };
